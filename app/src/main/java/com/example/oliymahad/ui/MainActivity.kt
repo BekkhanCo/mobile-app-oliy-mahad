cce85edaa4248aa21be69c95422c08937fee9436
imBOB <bekzodjon_valiev@epam.com>
2022-07-25 18:10:13 +0500

    branches connected

--- app/src/main/java/com/example/oliymahad/ui/MainActivity.kt
+++ app/src/main/java/com/example/oliymahad/ui/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.example.oliymahad.ui
 
 import android.annotation.SuppressLint
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import com.example.oliymahad.R
 import com.example.oliymahad.databinding.ActivityMainBinding
+import com.example.oliymahad.ui.fragments.CourseFragment
+import com.example.oliymahad.ui.fragments.EventFragment
+import com.example.oliymahad.ui.fragments.MyCourseFragment
+import com.example.oliymahad.ui.fragments.NotificationFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
 
 @SuppressLint("StaticFieldLeak")
 private lateinit var binding: ActivityMainBinding
@@ -15,5 +22,27 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(LayoutInflater.from(this))
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        binding.bottomNavBar.setOnNavigationItemSelectedListener(navListener)
+        supportFragmentManager.beginTransaction().replace(R.id.fragmentContainer, EventFragment())
+            .commit()
     }
+
+    private val navListener =
+        BottomNavigationView.OnNavigationItemSelectedListener { item ->
+            var selectedFragment: Fragment? = EventFragment()
+            when (item.itemId) {
+                R.id.btm_events -> selectedFragment = EventFragment()
+                R.id.btm_courses -> selectedFragment = CourseFragment()
+                R.id.btm_my_courses -> selectedFragment = MyCourseFragment()
+                R.id.btm_notification -> selectedFragment = NotificationFragment()
+            }
+            if (selectedFragment != null) {
+                supportFragmentManager
+                    .beginTransaction()
+                    .replace(R.id.fragmentContainer, selectedFragment)
+                    .commit()
+            }
+            true
+        }
+
 }
---
+++ app/src/main/java/com/example/oliymahad/ui/fragments/EventFragment.kt
@@ -0,0 +1,18 @@
+package com.example.oliymahad.ui.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.oliymahad.R
+
+class EventFragment:Fragment(R.layout.events_fragment) {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+}
