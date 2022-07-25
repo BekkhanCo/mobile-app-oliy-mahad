727683df187505cd6e2e14a62f5812bd268b6556
javohir-ergashboyev <ergashboyevjavohirbek@gmail.com>
2022-07-25 21:50:32 +0500

    unavailable code

--- app/src/main/java/com/example/oliymahad/ui/SplashActivity.kt
+++ app/src/main/java/com/example/oliymahad/ui/SplashActivity.kt
@@ -1,8 +1,10 @@
 package com.example.oliymahad.ui
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.oliymahad.R
+
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/com/example/oliymahad/utils/Constants.kt
+++ app/src/main/java/com/example/oliymahad/utils/Constants.kt
@@ -1,4 +1,7 @@
 package com.example.oliymahad.utils
 
 class Constants {
+    companion object{
+
+    }
 }
