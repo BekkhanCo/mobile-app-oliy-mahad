814e91abd0d1a2adde9cda4bee69c0010e81332c
javohir-ergashboyev <ergashboyevjavohirbek@gmail.com>
2022-07-26 16:03:28 +0500

    Adapter created

---
+++ app/src/main/java/com/example/oliymahad/adapter/CourseAdapter.kt
@@ -0,0 +1,34 @@
+package com.example.oliymahad.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.oliymahad.databinding.CoursesItemBinding
+import com.example.oliymahad.model.CourseModel
+
+class CourseAdapter(private val list: ArrayList<CourseModel>, val context: Context) :
+    RecyclerView.Adapter<CourseAdapter.Holder>() {
+    inner class Holder(private val binding: CoursesItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+            fun bind(pos:Int){
+                val currentItem=list[pos]
+            }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
+        return Holder(
+            CoursesItemBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: Holder, position: Int) {
+        holder.bind(position)
+    }
+
+    override fun getItemCount(): Int = list.size
+}
